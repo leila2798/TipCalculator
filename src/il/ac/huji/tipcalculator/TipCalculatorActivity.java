19f52e75997f790ffb025bebde61d4e727fb5f74
leila2798 <leila2798@gmail.com>
2014-02-22 17:19:37 +0200

    initial commit. simple layout

---
+++ src/il/ac/huji/tipcalculator/TipCalculatorActivity.java
@@ -0,0 +1,40 @@
+package il.ac.huji.tipcalculator;
+
+import java.text.DecimalFormat;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class TipCalculatorActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_tip_calculator);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.tip_calculator, menu);
+		return true;
+	}
+	
+	/** Called when the user clicks the Calculate button */
+	public void calculateTip(View view) {
+		EditText edtBillAmount = (EditText) findViewById(R.id.edtBillAmount);
+		double amount = Double.parseDouble(edtBillAmount.getText().toString());
+		CheckBox chkRound = (CheckBox) findViewById(R.id.chkRound);
+		boolean round = chkRound.isChecked();
+		TextView txtTipResult = (TextView) findViewById(R.id.txtTipResult);
+		DecimalFormat df = new DecimalFormat("#.##");
+		String tipResult = round? Integer.toString((int)Math.round(amount*0.12)): df.format(amount*0.12);
+		txtTipResult.setText("Tip: " + tipResult+"$");
+	}
+
+}
